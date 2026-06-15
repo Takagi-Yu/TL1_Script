f086c35ea1191c5f645d2bedbcb5dbab7875b1d9
yuu takagi <k024g0049@g.neec.ac.jp>
2026-06-15 12:11:24 +0900

    Update level_editor.py
    01_05

--- level_editor.py
+++ level_editor.py
@@ -39,9 +39,52 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
     bl_description = "拡張メニュー by " + bl_info["author"]
 
     def draw(self, context):
-        self.layout.operator("wm.url_open_preset",text="Manual",icon='HELP')
+
+        self.layout.operator(
+            MYADDON_OT_create_ico_sphere.bl_idname,
+            text = MYADDON_OT_create_ico_sphere.bl_label
+        )
+
+        self.layout.operator(MYADDON_OT_stretch_vertex.bl_idname, 
+                             text = MYADDON_OT_stretch_vertex.bl_label)
 
     def submenu(self,context):
         self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
 
-classes =(TOPBAR_MT_my_menu,)
+#オペレータ 頂点を伸ばす
+class MYADDON_OT_stretch_vertex(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_stretch_vertex"
+    bl_label = "頂点を伸ばす"
+    bl_description = "頂点座標を引っ張って伸ばします"
+    #リドゥ、アンドゥ可能オプション
+    bl_options = {'REGISTER', 'UNDO'}
+
+    #メニューを実行したときに呼ばれるコールバック関数
+    def execute(self, context):
+        bpy.data.objects["Cube"].data.vertices[0].co.x += 1.0
+        print("頂点を伸ばしました。")
+            
+        #オペレータの命令終了を通知
+        return {'FINISHED'}
+    
+#オペレータ ICO球生成
+class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_create_object"
+    bl_label = "ICO球生成"
+    bl_description = "ICO球を生成します"
+    #リドゥ、アンドゥ可能オプション
+    bl_options = {'REGISTER', 'UNDO'}
+
+    #メニューを実行したときに呼ばれるコールバック関数
+    def execute(self, context):
+        bpy.ops.mesh.primitive_ico_sphere_add()
+        print("ICO球を生成しました。")
+            
+        #オペレータの命令終了を通知
+        return {'FINISHED'}
+
+classes =(
+    MYADDON_OT_create_ico_sphere,
+    MYADDON_OT_stretch_vertex,
+    TOPBAR_MT_my_menu,
+)
